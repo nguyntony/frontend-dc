36ee1d0541e500e70ab0ddb6de49979f5c63c6b3
Draines1 <rainesdarren@gmail.com>
2020-11-16 14:25:12 -0500

    google api

--- modules/place.js
+++ modules/place.js
@@ -1,1 +1,4 @@
+import ajax from "./ajax.js";
+import { google_api_key, corsFix } from "../config.js"
 
+export let google = ajax(corsFix + `AIzaSyDalbPmW4QbIZb913EPmkGud6TsA6QsUjk=${google_api_key}`,)
