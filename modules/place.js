23699686673fd6dfd005ba3279099a041ea4c169
Chad Reynolds <chad.s.reynolds@gmail.com>
2020-11-16 15:09:25 -0500

    updates

--- modules/place.js
+++ modules/place.js
@@ -1,4 +1,6 @@
 import ajax from "./ajax.js";
 import { google_api_key, corsFix } from "../config.js"
 
-export let google = ajax(corsFix + `https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&key==${google_api_key}`)
+export let google = ajax(corsFix + `https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&key=${google_api_key}`)
+
+//need to pass ${search} in place of 123+main+street in above url, removed extra '=' from before api key insertion
