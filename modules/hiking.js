a5f50587101de4a5e0d047adb2235951793fc022
Chad Reynolds <chad.s.reynolds@gmail.com>
2020-11-16 16:46:14 -0500

    day 1

--- modules/hiking.js
+++
@@ -1,7 +0,0 @@
-import ajax from "./ajax.js";
-import { hiking_api_key, corsFix } from "../config.js"
-import { makeCard } from "./makeCard.js"
-
-export let hiking = ajax(corsFix + `https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${hiking_api_key}`, makeCard)
-
-//need to pass ${lat} and ${lon} into above url from google places API
